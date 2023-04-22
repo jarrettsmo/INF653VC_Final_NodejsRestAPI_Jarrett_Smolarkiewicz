6ae8c82b168e9ba39a849c6dcb1f358c9fb7b8cf
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-04-21 23:46:52 -0500

    Added project requirements.

--- INF653VC_Final_Project_JarrettSmolarkiewicz/server.js
+++ INF653VC_Final_Project_JarrettSmolarkiewicz/server.js
@@ -1,2 +1,19 @@
+// Final Project Requirements:
+//  1) Link to GitHub code repository.
+//  2) Link to deployed project.
+//  3) One page PDF document discussing what challenges you faced while building your project.
+//  4) Automated Tests for REST API (Automated Testing page will run 70 detailed automated tests on REST API).
+
+// Final Project GitHub Repository (Node.js REST API)
 // https://github.com/jarrettsmo/INF653VC_Final_NodejsRestAPI_Jarrett_Smolarkiewicz
+
+// Final Project Automated Tests
+// https://dazzling-snickerdoodle-777101.netlify.app/
+
+// ----------------------> FINAL PROJECT RESOURCES <----------------------
+// Dave Gray YouTube Playlist for Vanilla JavaScript
+// https://www.youtube.com/playlist?list=PL0Zuz27SZ-6Oi6xNtL_fwCrwpuqylMsgT 
+
+// Dave Gray YouTube Playlist for Node.js
+// https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw 
 
