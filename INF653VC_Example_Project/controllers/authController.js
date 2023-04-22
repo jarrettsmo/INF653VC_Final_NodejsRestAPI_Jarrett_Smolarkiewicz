756fde7e9c482313466bfb662d65f2a942b74817
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-04-22 10:27:23 -0500

    Updated controller files.

--- INF653VC_Example_Project/controllers/authController.js
+++ INF653VC_Example_Project/controllers/authController.js
@@ -1,12 +1,13 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
-
+// const usersDB = {
+//     users: require('../model/users.json'),
+//     setUsers: function (data) { this.users = data }
+// }
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const fsPromises = require('fs').promises;
-const path = require('path');
+// const fsPromises = require('fs').promises;
+// const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
