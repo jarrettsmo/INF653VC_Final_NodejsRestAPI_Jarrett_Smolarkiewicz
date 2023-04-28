a268b893315fe0864d8fd33667e5beacc9f5463c
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-04-27 23:30:08 -0500

    Ensured correct URL's allowed.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://inf-653-vc-final-project-jarrett-smolarkiewicz.glitch.me/',
+    'https://replit.com/@JarrettSmo/INF653VCFinalNodejsRestAPIJarrettSmolarkiewicz',
     'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
